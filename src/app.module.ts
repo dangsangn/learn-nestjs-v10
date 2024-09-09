58d24bcc2e8c23000bdc5d13e36ada7e62eeb559
sang.nguyendang <dangsangnguyen260399@gmail.com>
2024-09-09 09:42:44 +0000

    step 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 import { SongsModule } from './songs/songs.module';
-
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController);
+  }
+}
