b19302fb073cb83ea57293ea88f38558d2274078
sang.nguyendang <dangsangn6147@gmail.com>
2024-09-29 22:19:11 +0700

    create module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
-import helmet from 'helmet';
-import * as compression from 'compression';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
@@ -10,15 +7,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
 
-  //register all plugins and extension
-  app.enableCors({ origin: '*' });
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableVersioning({ type: VersioningType.URI });
-  app.use(helmet());
-  app.use(compression());
-
-  await app.listen(port, () => {
-    console.log(`Application running at port ${port}`);
-  });
+  await app.listen(port);
 }
 bootstrap();
