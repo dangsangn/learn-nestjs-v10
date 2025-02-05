74e75682e9088d5c71bd489bba3054e104e814fa
SangND-Kozocom <sangnd@kozo-japan.com>
2025-02-05 16:00:27 +0700

    .

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,6 +14,11 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.email, sub: user._id };
     return {
+      user: {
+        id: user._id,
+        email: user.email,
+        name: user.name,
+      },
       access_token: this.jwtService.sign(payload),
     };
   }
